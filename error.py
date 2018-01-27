c0ab58b71b12e62fc70651f4e2fb08ff96b550fc
Mick Lee <micklwl@hotmail.com>
2018-01-28 00:32:26 +0800

    left rs

--- error.py
+++ error.py
@@ -1,15 +1,18 @@
 
 def error(text):
-	text.replace(" ","");
+	text = text.replace(" ","");
 	letters = 0
 	leftBracket = 0
 	rightBracket = 0
 	length = len(text)
+	list = ['p','q','r','s','v','~','(',')','^']
 	for n in range(0,length, 1):
 		current = text[n]
-		if current.isalpha():
+		if current not in list:
+			return 1
+		if current.isalpha() and current != 'v':
 			if current != 'p' and current != 'q' and current != 'r' and current != 's':
-				return 16
+				return 1
 		if n < length - 1:
 			after = text[n+1]
 		if n > 0:
@@ -21,41 +24,41 @@ def error(text):
 					return 1
 		if n == 0 or n == length - 1:
 			if current == 'v' or current == '^':
-				return 2
+				return 1
 		if n == length - 1:
 			if current == '~':
-				return 14
+				return 1
 		if current == '~':
 			if n > 0:
 				if previous == 'p' or previous == 'q' or previous == 'r' or previous == 's':
-					return 3
+					return 1
 				if after == 'v' or after == '^': 
-					return 4
+					return 1
 		if current == 'v' or current == '^':
 			if previous != 'p' and previous != 'q' and previous != 'r' and previous != 's':
-				return 5
+				return 1
 			if after == 'v' or after == '^' or after == ')': 
-				return 6		
+				return 1		
 		if current == '(':
 			if n == length-1 or n == length -2:
-				return 7
+				return 1
 			if n > 0:
 				if previous != 'v' and previous != '^' and previous != '(' and previous != '~':
-					return 8
+					return 1
 			if after == ')' or after == 'v' or after == '^':
-				return 9
+				return 1
 			leftBracket += 1;
 		if current == ')':
 			if n == 0 or n == 1:
-				return 10
+				return 1
 			if previous == '~' or previous == 'v' or previous == '^':
-				return 11
+				return 1
 			if after != 'v' and after != '^' and after != ')':
-				return 12
+				return 1
 			if n == length - 2 and after != ')':
-				return 15
+				return 1
 			rightBracket += 1
 	if letters == 0 or letters == length or leftBracket != rightBracket:
-		return 13
+		return 1
 	else:
 		return 0
