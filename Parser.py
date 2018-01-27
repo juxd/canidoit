6ed30573cfbf1f708333cd7fdc40e67c772e979d
Ju <jluuiss@gmail.com>
2018-01-28 01:07:42 +0800

    fixed ordering parameters

--- Parser.py
+++ Parser.py
@@ -1,22 +1,22 @@
 
 # text should be a function that takes in an array of booleans and returns a boolean
 def textToLogic(text):
-  print(text)
   text = text.lower()
   textAsChars = list(text)
+  
   def replaceChar(char1, char2):
     for i in range(0, len(textAsChars)):
       if textAsChars[i] == char1:
         textAsChars[i] = char2
-  replaceChar('p', 'params[0]')
-  replaceChar('q', 'params[1]')
-  replaceChar('r', 'params[2]')
-  replaceChar('s', 'params[3]')
+  charCount = 0;
+  for i in range(0, 4):
+    if any(ord(j) == 112 + i for j in text):
+      replaceChar(chr(112 + i), 'params[' + str(charCount) + ']')
+      charCount += 1
   replaceChar('~', ' not ')
   replaceChar('^', ' and ')
   replaceChar('v', ' or  ')
   result = ''.join(textAsChars)
-  print(result)
   return result
 
 # takes in a predicate in text and returns an array of parameters/variables in it
--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telegram
 import config
 import logging
 from telegram.ext import Updater, CommandHandler
-import LogicHandler, Parser
+import LogicHandler, Parser, error
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -16,7 +16,7 @@ def start(bot, update):
 
 def handleInput(bot, update):
   msgText = update.message.text.replace("/parsethis", "")
-  if !Error.error(msgText):
+  if !(error.error(msgText)):
      bot.send_message(chat_id=update.message.chat_id, text = "Error in Notation! Tray Again!",
                     parse_mode="Markdown")
   else:
@@ -25,6 +25,15 @@ def handleInput(bot, update):
     res = LogicHandler.getTable(predicateFn, params)
     bot.send_message(chat_id=update.message.chat_id, text = "```" +res+"```",
                     parse_mode="Markdown")
+
+# def handleComparison(bot, update):
+#   sentMsg = bot.send_msg(chat_id=update.message.chat_id, text="Reply me the two predicates to compare!")
+#   toCompare = []
+#   def getStmts(bot, update):
+#     if (update.message.reply_to_message == sentMsg):
+#       toCompare = 
+#   dispatcher.add_handler(
+
 
 startHandler = CommandHandler('start', start)
 inputHandler = CommandHandler('parsethis', handleInput)
